736ea500a2fd0f88933805f10697d98f30c6dcd5
Alexander Whitaker <whitaker.alexander@gmail.com>
2020-08-04 21:29:45 -0500

    Set Content-Type to application/json

--- src/wishlist_api/handler.clj
+++ src/wishlist_api/handler.clj
@@ -1,13 +1,36 @@
 (ns wishlist-api.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [clojure.data.json :as json]))
 
 (defn uuid [] (str (java.util.UUID/randomUUID)))
+(defn get-time [] (.getTime (java.util.Date.)))
+
+
+(defn get-items []
+  [{ :id (uuid)
+     :name "Legos"
+     :created (get-time)
+     :lastUpdated (get-time)
+     :purchased nil},
+   { :id (uuid)
+     :name "Coffee"
+     :created (get-time)
+     :lastUpdated (get-time)
+     :purchase { :userId (uuid)
+                   :timestamp (get-time)
+                   :name "Alex"}}])
+
+(defn json-success [body]
+  {:status 200
+   :headers { "Content-Type" "application/json"}
+   :body (json/write-str { :data body })})
+
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (GET "/:id" [id] (uuid))
+  (GET "/:id" [id] (json-success (get-items)))
   (route/not-found "Not Found"))
 
 (def app
